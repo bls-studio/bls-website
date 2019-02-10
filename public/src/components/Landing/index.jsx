10bebc88f4ec6bfc392039b895feb0af1d34ce03
blake-swl <seulwoolee93@gmail.com>
2019-02-09 17:56:22 -0800

    [edit] parparallax

--- public/src/components/Landing/index.jsx
+++ public/src/components/Landing/index.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import './home.scss';
 
 const Home = () => (
-  <div id="home">
+  <div id="home" className="parallax__group">
+    <div className="parallax">
+      <div className="parallax__layer parallax__layer--back">
+        <div className="parallax__overlay"></div>
+        {/* <div>sample background</div> */}
+      </div>
+      <div className="parallax__layer parallax__layer--base">
+        <h3 className="tagline">A creative web development agency.</h3>
+      </div>
+    </div>
+
+  </div>
+);
+
+export default Home;
+  {/* <div id="home">
     <div className="home-container">
       <div className="home-left">
         <h3 className="tagline">A creative web development agency.</h3>
@@ -13,7 +28,4 @@ const Home = () => (
         </div>
       </div>
     </div>
-  </div>
-);
-
-export default Home;
+  </div> */}
