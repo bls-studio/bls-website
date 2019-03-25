99b4c18f4ca1c268f2f25dc9a5f8d80845ef80b6
blake-swl <seulwoolee93@gmail.com>
2019-03-24 21:32:10 -0700

    [edit] parallax

--- public/src/components/App/index.jsx
+++ public/src/components/App/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import './global.scss';
+
 // Componenets
 import Navbar from '../Navbar';
 import Home from '../Landing';
 import About from '../About';
 
 const App = () => (
-  <div>
-    <Navbar />
-    <Home />
-    <About />
+  <div className="parallax">
+    <Navbar/>
+    <Home className="parallax__group"/>
+    <About className="parallax__group"/>
   </div>
 );
 
--- public/src/components/Landing/index.jsx
+++ public/src/components/Landing/index.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './home.scss';
 
+
 const Home = () => (
-  <div id="home" className="parallax__group">
-    <div className="parallax">
-      <div className="parallax__layer parallax__layer--back">
-        <div className="parallax__overlay"></div>
-      </div>
-      <div className="parallax__layer parallax__layer--base">
-        <h3 className="tagline">A creative web development studio.</h3>
-      </div>
-      <div className="home-static">
-        <p className="home-tagline">Connecting through code, design, & innovation.</p>
-      </div>
-    </div>
-    {/* <div className="home-background-overlap"></div> */}
+  <div id="home">
+
   </div>
 );
+// const Home = () => (
+//   <div id="home">
+//     <div className="parallax">
+//       <div className="parallax__layer parallax__layer--back home-image">
+//         <div className="parallax__overlay"></div>
+//       </div>
+//       <div className="parallax__layer parallax__layer--base">
+//         <h3 className="tagline">A creative web development studio.</h3>
+//       </div>
+//       <div className="home-static">
+//         <p className="home-tagline">Connecting through code, design, & innovation.</p>
+//       </div>
+//     </div>
+//     {/* <div className="home-background-overlap"></div> */}
+//   </div>
+// );
 
 export default Home;
